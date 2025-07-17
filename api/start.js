d2214aae91508ed1e4baf6120778ba106aedadcd
IDebugger <141814641+IntellsGamer@users.noreply.github.com>
2025-07-17 19:16:21 +0330

    Update start.js

--- api/start.js
+++ api/start.js
@@ -56,10 +56,10 @@ console.log('✅ Slash command registered');
 
 // Keep the connection alive for ~10 seconds before closing
 setTimeout(() => {
-console.log('⌛ Bot shutting down after 10 minutes.');
+console.log('⌛ Bot shutting down after 24 hours.');
 client.destroy();
-res.end('Bot stopped after 10 minutes.');
-}, 600000);
+res.end('Bot stopped after 24 hours.');
+}, 86400000);
 
 } catch (error) {
 console.error(error);
